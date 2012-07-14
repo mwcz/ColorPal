735949b0cb4c63bf447fa7b087333b497b36bab1
mwcz <mwc@clayto.org>
2012-07-14 11:35:50 -0400

    reduce collision odds for js vars and libs ($)

--- js/cp-events.js
+++ js/cp-events.js
@@ -1,150 +1,144 @@
-(function() {
+(function($) {
 
+    var ctx,
+        ctx_small,
+        canvas,
+        canvas_small,
+        mc,
+        img_width           = 0,
+        img_height          = 0,
+        max_canvas_width    = 468,
+        canvas_small_height = 128,
+        canvas_small_width  = 128;
 
-var ctx,
-    ctx_small,
-    canvas,
-    canvas_small,
-    mc,
-    img_width           = 0,
-    img_height          = 0,
-    max_canvas_width    = 468,
-    canvas_small_height = 128,
-    canvas_small_width  = 128;
+    function handle_file_select( evt ) {
 
-function handle_file_select( evt ) {
+        if( evt.stopPropagation ) evt.stopPropagation();
+        if( evt.preventDefault  ) evt.preventDefault();
 
-    if( evt.stopPropagation ) evt.stopPropagation();
-    if( evt.preventDefault  ) evt.preventDefault();
+        var files = evt.dataTransfer.files; // FileList object
 
-    var files = evt.dataTransfer.files; // FileList object
+        // Loop through the FileList and render image files as thumbnails.
+        for ( var i = 0, f; f = files[i]; i++ ) {
 
-    // Loop through the FileList and render image files as thumbnails.
-    for ( var i = 0, f; f = files[i]; i++ ) {
-
-        // Only process image files.
-        if (!f.type.match('image.*')) {
-            continue;
-        }
-
-        var reader       = new FileReader();
-        var img          = document.createElement("img"); // display-size image
-        var img_small    = document.createElement("img"); // smaller image for faster median cut
-
-        img.onload = function() {
-
-            var img_ratio     = img.width / img.height;
-
-            var canvas_width  = ( img.width  > max_canvas_width  ) ? ( img_ratio > 1 ) ? max_canvas_width  : max_canvas_width*img_ratio  : img.width;
-            var canvas_height = Math.floor( canvas_width / img_ratio );
-            // Resize the canvas to the image size
-            canvas.width             = canvas_width;
-            canvas.height            = canvas_height;
-
-            // Resize the canvas with CSS to trigger CSS3 transitions
-            canvas.style.width       = canvas_width + "px";
-            canvas.style.height      = canvas_height + "px";
-
-            // Draw the downsized image inside the canvas
-            ctx_small.drawImage( img, 0, 0, canvas_small_width, canvas_small_height );
-
-            var data = ctx_small.getImageData( 0, 0, canvas_small_width, canvas_small_height );
-            var rgbdata = imagedata_to_rgb( data );
-            mc = MedianCut();
-            mc.init( rgbdata );
-            var palette = mc.get_fixed_size_palette( 8 );
-
-            // clear any previous swatches
-
-            for( var ic = palette.length - 1; ic >= 0; ic-- ) {
-                var color = palette[ic];
-                var hex = farb.pack( [ color[0]/255, color[1]/255, color[2]/255 ] );
-                CP_ViewModel.colors()[ic].hex(hex);
-                //var sp = create_swatch( ic, color );
-                //swatches.appendChild( sp );
+            // Only process image files.
+            if (!f.type.match('image.*')) {
+                continue;
             }
 
-            // Draw the display image
-            ctx.drawImage( img, 0, 0, canvas_width, canvas_height );
+            var reader       = new FileReader();
+            var img          = document.createElement("img"); // display-size image
+            var img_small    = document.createElement("img"); // smaller image for faster median cut
 
-        };
+            img.onload = function() {
 
-        // Read in the image file as a data URL.
-        reader.readAsDataURL(f);
+                var img_ratio     = img.width / img.height;
 
-        reader.onload = function(e) {
+                var canvas_width  = ( img.width  > max_canvas_width  ) ? ( img_ratio > 1 ) ? max_canvas_width  : max_canvas_width*img_ratio  : img.width;
+                var canvas_height = Math.floor( canvas_width / img_ratio );
+                // Resize the canvas to the image size
+                canvas.width             = canvas_width;
+                canvas.height            = canvas_height;
 
-            // Assign the image's src to the filesystem image's data URL
-            // [http://en.wikipedia.org/wiki/Data_URI_scheme]
-            img.src = e.target.result; 
+                // Resize the canvas with CSS to trigger CSS3 transitions
+                canvas.style.width       = canvas_width + "px";
+                canvas.style.height      = canvas_height + "px";
 
-        };
+                // Draw the downsized image inside the canvas
+                ctx_small.drawImage( img, 0, 0, canvas_small_width, canvas_small_height );
+
+                var data = ctx_small.getImageData( 0, 0, canvas_small_width, canvas_small_height );
+                var rgbdata = imagedata_to_rgb( data );
+                mc = MedianCut();
+                mc.init( rgbdata );
+                var palette = mc.get_fixed_size_palette( 8 );
+
+                // clear any previous swatches
+
+                for( var ic = palette.length - 1; ic >= 0; ic-- ) {
+                    var color = palette[ic];
+                    var hex = ColorPal_farb.pack( [ color[0]/255, color[1]/255, color[2]/255 ] );
+                    ColorPal_ViewModel.colors()[ic].hex(hex);
+                    //var sp = create_swatch( ic, color );
+                    //swatches.appendChild( sp );
+                }
+
+                // Draw the display image
+                ctx.drawImage( img, 0, 0, canvas_width, canvas_height );
+
+            };
+
+            // Read in the image file as a data URL.
+            reader.readAsDataURL(f);
+
+            reader.onload = function(e) {
+
+                // Assign the image's src to the filesystem image's data URL
+                // [http://en.wikipedia.org/wiki/Data_URI_scheme]
+                img.src = e.target.result; 
+
+            };
+        }
     }
-}
 
 
-// Drag and drop event handlers
-function file_drag_start( e ) {
+    // Drag and drop event handlers
+    function file_drag_start( e ) {
 
-    if( e.stopPropagation ) {
-        e.stopPropagation()
+        if( e.stopPropagation ) {
+            e.stopPropagation()
+        }
+
+        return false;
     }
 
-    return false;
-}
+    function file_drag_enter( e ) {
+    }
 
-function file_drag_enter( e ) {
-}
+    function file_drag_over( e ) {
+        e.stopPropagation();
+        e.preventDefault();
+        e.dataTransfer.dropEffect="copy";
+        $(canvas).addClass('cp-canvas-drag-over');
+    }
 
-function file_drag_over( e ) {
-    e.stopPropagation();
-    e.preventDefault();
-    e.dataTransfer.dropEffect="copy";
-    $(canvas).addClass('cp-canvas-drag-over');
-}
+    function file_drag_leave( e ) {
+        e.stopPropagation();
+        e.preventDefault();
+        e.dataTransfer.dropEffect="copy";
+        $(canvas).removeClass('cp-canvas-drag-over');
+    }
 
-function file_drag_leave( e ) {
-    e.stopPropagation();
-    e.preventDefault();
-    e.dataTransfer.dropEffect="copy";
-    $(canvas).removeClass('cp-canvas-drag-over');
-}
+    function file_drag_end  ( e ) {
+    }
 
-function file_drag_end  ( e ) {
-}
+    $(window).load( function() {
+        document.getElementById("cp-input-image").onchange = handle_file_select;
 
+        canvas              = document.getElementById("cp-canvas");
 
+        canvas_small        = document.createElement("canvas");
+        canvas_small.width  = canvas_small_width;
+        canvas_small.height = canvas_small_height;
 
+        ctx                 = canvas.getContext("2d");
+        ctx_small           = canvas_small.getContext("2d");
 
+        // Set up drag handles
+        var image_drop_zone = document.getElementById("cp-canvas");
+        image_drop_zone.addEventListener( "dragstart", file_drag_start   , false );
+        image_drop_zone.addEventListener( "dragenter", file_drag_enter   , false );
+        image_drop_zone.addEventListener( "dragover" , file_drag_over    , false );
+        image_drop_zone.addEventListener( "dragleave", file_drag_leave   , false );
+        image_drop_zone.addEventListener( "drop"     , handle_file_select, false );
+        image_drop_zone.addEventListener( "dragend"  , file_drag_end     , false );
 
-$(window).load( function() {
-    document.getElementById("cp-input-image").onchange = handle_file_select;
-
-    canvas              = document.getElementById("cp-canvas");
-
-    canvas_small        = document.createElement("canvas");
-    canvas_small.width  = canvas_small_width;
-    canvas_small.height = canvas_small_height;
-
-    ctx                 = canvas.getContext("2d");
-    ctx_small           = canvas_small.getContext("2d");
-
-    // Set up drag handles
-    var image_drop_zone = document.getElementById("cp-canvas");
-    image_drop_zone.addEventListener( "dragstart", file_drag_start   , false );
-    image_drop_zone.addEventListener( "dragenter", file_drag_enter   , false );
-    image_drop_zone.addEventListener( "dragover" , file_drag_over    , false );
-    image_drop_zone.addEventListener( "dragleave", file_drag_leave   , false );
-    image_drop_zone.addEventListener( "drop"     , handle_file_select, false );
-    image_drop_zone.addEventListener( "dragend"  , file_drag_end     , false );
-
-    /* TODO: enable file input selection of images (esp. for mobile)
-    var file_input = document.getElementById("cp-input-image");
-    file_input.addEventListener( "change", handle_file_select, false );
-    */
+        /* TODO: enable file input selection of images (esp. for mobile)
+        var file_input = document.getElementById("cp-input-image");
+        file_input.addEventListener( "change", handle_file_select, false );
+        */
 
 
-} );
+    } );
 
-
-}());
+})(jQuery);
--- js/cp-knockout.js
+++ js/cp-knockout.js
@@ -1,71 +1,70 @@
-var farb, pick, selected, CP_ViewModel;
-$(document).ready(function() {
+var ColorPal_farb, ColorPal_pick, ColorPal_selected, ColorPal_ViewModel;
+(function($) {
+    $(document).ready(function() {
 
 
-    function CP_ViewModelFn() {
-        var self = this;
+        function ColorPal_ViewModelFn() {
+            var self = this;
 
-        self.colors = ko.observableArray( [
-            new CP_Color("#000"), new CP_Color("#000"),
-            new CP_Color("#000"), new CP_Color("#000"),
-            new CP_Color("#000"), new CP_Color("#000"),
-            new CP_Color("#000"), new CP_Color("#000"),
-        ]);
+            self.colors = ko.observableArray( [
+                new CP_Color("#000"), new CP_Color("#000"),
+                new CP_Color("#000"), new CP_Color("#000"),
+                new CP_Color("#000"), new CP_Color("#000"),
+                new CP_Color("#000"), new CP_Color("#000"),
+            ]);
 
-        self.selected = ko.observable(false);
+            self.selected = ko.observable(false);
 
-        self.palette_created = ko.computed( function() {
+            self.palette_created = ko.computed( function() {
 
-            var i,
-                is_default_palette = true;
+                var i,
+                    is_default_palette = true;
 
-            for( i = self.colors().length - 1; i >= 0; --i ) {
-                if ( self.colors()[i].hex() != "#000" ) {
-                    is_default_palette = false;
+                for( i = self.colors().length - 1; i >= 0; --i ) {
+                    if ( self.colors()[i].hex() != "#000" ) {
+                        is_default_palette = false;
+                    }
                 }
-            }
 
-            return !is_default_palette;
+                return !is_default_palette;
 
-			// yes, this means pasting in a pure black image
-			// will not cause swatches to appear.  I'm okay with that :)
+                // yes, this means pasting in a pure black image
+                // will not cause swatches to appear.  I'm okay with that :)
 
-        });
+            });
 
-    };
-
-    CP_ViewModel = new CP_ViewModelFn;
-
-    ko.applyBindings( CP_ViewModel );
-
-    function CP_Color( hex ) {
-        return {
-            hex: ko.observable(hex)
         };
-    }
 
-    farb = $.farbtastic('#cp-picker');
-    farb.linkTo(function( hex ) {
-        if( selected ) {
-            var swatch_id = $(selected).attr('id').slice(-1);
-            CP_ViewModel.colors()[ swatch_id ].hex( hex );
+        ColorPal_ViewModel = new ColorPal_ViewModelFn;
+
+        ko.applyBindings( ColorPal_ViewModel );
+
+        function CP_Color( hex ) {
+            return {
+                hex: ko.observable(hex)
+            };
         }
-    });
-    pick = $('#cp-picker');
-    selected;
-    $('.cp-swatch')
-        .click(function() {
-            if (selected) {
-                $(selected).removeClass('cp-swatch-selected');
+
+        ColorPal_farb = $.farbtastic('#cp-picker');
+        ColorPal_farb.linkTo(function( hex ) {
+            if( ColorPal_selected ) {
+                var swatch_id = $(ColorPal_selected).attr('id').slice(-1);
+                ColorPal_ViewModel.colors()[ swatch_id ].hex( hex );
             }
-            if ( selected == this ) {
+        });
+        ColorPal_pick = $('#cp-picker');
+        $('.cp-swatch').click(function() {
+            if (ColorPal_selected) {
+                $(ColorPal_selected).removeClass('cp-swatch-selected');
+            }
+            if ( ColorPal_selected == this ) {
 
                 // make swatch look deselected
                 $(this).removeClass('cp-swatch-selected');
 
                 // now nothing is selected
-                selected = false;
-                CP_ViewModel.selected(false);
+                ColorPal_selected = false;
+                ColorPal_ViewModel.selected(false);
             } else {
 
                 // get the swatch number from the id "cp-swatch-2", etc.
@@ -73,15 +72,16 @@ $(document).ready(function() {
                 var swatch_num = $(this).attr('id').slice(-1);
 
                 // make swatch look selected
-                $(selected = this).addClass('cp-swatch-selected');
-                CP_ViewModel.selected(true);
+                $(ColorPal_selected = this).addClass('cp-swatch-selected');
+                ColorPal_ViewModel.selected(true);
 
                 // set the colorpicker to this swatch's color. (note that
                 // this will cause the colorpicker's callback to fire)
-                farb.setColor( CP_ViewModel.colors()[ swatch_num ].hex() );
+                ColorPal_farb.setColor( ColorPal_ViewModel.colors()[ swatch_num ].hex() );
             }
         });
 
-});
+    });
+})(jQuery);
 
 
